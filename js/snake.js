0135a5a6847d8122dd1696bcc892107a44b2df96
Clarke Lamothe <clarkelamothe@yahoo.fr>
2021-01-13 04:50:33 -0300

    Inputs, Snake, Game implementations

---
+++ js/snake.js
@@ -0,0 +1,28 @@
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
+const snakeBody = [
+    { x: 11, y: 11 }
+]
+
+
+export function update() {
+    const inputDirection = getInputDirection();
+    console.log('update snake');
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] };
+    }
+
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach((segment) => {
+        const snakeElement = document.createElement('div');
+        snakeElement.style.gridColumnStart = segment.x;
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.classList.add('snake');
+        gameBoard.appendChild(snakeElement);
+    })
+}
